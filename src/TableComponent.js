f86d883096aa959748f9c15ded0984e1c4186b84
Julia.Passynkova <julia.passynkova@nurun.com>
2016-08-16 00:07:48 -0400

    key fix

--- src/TableComponent.js
+++ src/TableComponent.js
@@ -20,11 +20,11 @@ class TableComponent extends Component {
                 {this.state.data.map(({name, description}, index) => {
                     let children = [];
                     children.push(
-                        <tr key={`name + ${index}`}>
+                        <tr key={`name-${index}`}>
                             <td>Start row: {name}</td>
                         </tr>);
                     children.push(
-                        <tr key={`description + ${index}`}>
+                        <tr key={`description-${index}`}>
                             <td>End row: {description}</td>
                         </tr>);
                     return children;
