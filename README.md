892c9a6df8ca02b7d555694b5b421b97baedb3fd
Julia.Passynkova <julia.passynkova@nurun.com>
2016-08-15 23:50:18 -0400

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+React way to do Angulat ng-repeat-start and ng-repeat-end
---
+++ src/TableComponent.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+class TableComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [
+                {name: 'julia', description: 'Girl'},
+                {name: 'steven', description: 'Boy'},
+                {name: 'anna', description: 'Girl'}
+
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <table>
+                <tbody>
+                {this.state.data.map(({name, description}, index) => {
+                    let children = [];
+                    children.push(
+                        <tr key={`name + ${index}`}>
+                            <td>Start row: {name}</td>
+                        </tr>);
+                    children.push(
+                        <tr key={`description + ${index}`}>
+                            <td>End row: {description}</td>
+                        </tr>);
+                    return children;
+                })}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default TableComponent;
